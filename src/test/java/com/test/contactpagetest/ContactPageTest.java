3ffd4e0165c2c60b43783aad19a1186bc120ca4f
nilu herath <niluherath@hotmail.com>
2025-03-26 09:38:05 +1300

    getting rid of webdriver manager for selenium 4.30.0 and update in tests to add title validation

--- src/test/java/com/test/contactpagetest/ContactPageTest.java
+++ src/test/java/com/test/contactpagetest/ContactPageTest.java
@@ -8,48 +8,51 @@ import org.testng.Assert;
 public class ContactPageTest extends BaseTest {
 
 
-     @org.testng.annotations.Test(description="This test validates the error messages on contact page")
-     public void testContactPageForm()
-     {
-     logger = extent.createTest("To verify contact page form error messages on mandatory fields");
-     given("user arrives on contact page");
-     when(" submit button on contact page is clicked");
-     HomePage homePage   = new HomePage(driver);
-     homePage.clickContactTab();
-     ContactPage contactPage = new ContactPage(driver);
-     contactPage.clickSubmitButton();
-     then(" error messages are shown on mandatory fields");
-     Assert.assertTrue(contactPage.validateForenameRequired());
-     Assert.assertTrue(contactPage.validateEmailRequired());
-     Assert.assertTrue(contactPage.validateMessageRequired());
-     contactPage.typeInForenameInputField(name);
-     contactPage.typeInEmailInputField(emailAddress);
-     contactPage.typeInMessageInputField(message);
-     Assert.assertFalse(contactPage.validateForenameRequired());
-     Assert.assertFalse(contactPage.validateEmailRequired());
-     Assert.assertFalse(contactPage.validateMessageRequired());
+    @org.testng.annotations.Test(description = "This test validates the error messages on contact page")
+    public void testContactPageFormErrorMessagesForMandatoryFields() {
+        logger = extent.createTest("To verify contact page form error messages on mandatory fields");
+        given(" user arrives on contact page");
+        when(" submit button on contact page is clicked without entering value for mandatory fields");
+        HomePage homePage = new HomePage(driver);
+        homePage.clickContactTab();
+        ContactPage contactPage = new ContactPage(driver);
+        Assert.assertEquals(contactPage.getTitle(), "Jupiter Toys");
+        contactPage.clickSubmitButton();
+        then(" error messages are shown on mandatory fields");
+        Assert.assertTrue(contactPage.validateForenameRequired());
+        Assert.assertTrue(contactPage.validateEmailRequired());
+        Assert.assertTrue(contactPage.validateMessageRequired());
+        and(" inputs for mandatory fields get populated");
+        contactPage.typeInForenameInputField(name);
+        contactPage.typeInEmailInputField(emailAddress);
+        contactPage.typeInMessageInputField(message);
+        then(" error messages get disappeared");
+        Assert.assertFalse(contactPage.validateForenameRequired());
+        Assert.assertFalse(contactPage.validateEmailRequired());
+        Assert.assertFalse(contactPage.validateMessageRequired());
 
-     }
-     @org.testng.annotations.Test(description="")
-     public void testContactPageFormSubmission()
-     {
-     logger = extent.createTest("To verify contact page form submission");
-     given("user arrives on contact page");
-     when(" submit button on contact page is clicked");
-     HomePage homePage   = new HomePage(driver);
-     homePage.clickContactTab();
-     ContactPage contactPage = new ContactPage(driver);
-     contactPage.clickSubmitButton();
-     then("a success message is shown");
+    }
 
-     contactPage.typeInForenameInputField(name);
-     contactPage.typeInEmailInputField(emailAddress);
-     contactPage.typeInMessageInputField(message);
-     contactPage.clickSubmitButton();
-     contactPage.waitForSuccessMessage();
+    @org.testng.annotations.Test(description = "")
+    public void testContactPageFormSucessfulSubmission() {
+        logger = extent.createTest("To verify contact page form submission sucessful submission");
+        given(" user arrives on contact page");
+        when(" submit button on contact page is clicked");
+        HomePage homePage = new HomePage(driver);
+        homePage.clickContactTab();
+        ContactPage contactPage = new ContactPage(driver);
+        Assert.assertEquals(contactPage.getTitle(), "Jupiter Toys");
+        contactPage.clickSubmitButton();
+        when(" mandatory fields values are entered");
+        contactPage.typeInForenameInputField(name);
+        contactPage.typeInEmailInputField(emailAddress);
+        contactPage.typeInMessageInputField(message);
+        contactPage.clickSubmitButton();
+        then(" a success message is shown");
+        contactPage.waitForSuccessMessage();
 
 
-     }
+    }
 
 
 }
--- src/test/java/com/test/homepagetest/HomePageTest.java
+++ src/test/java/com/test/homepagetest/HomePageTest.java
@@ -1,18 +1,16 @@
 package com.test.homepagetest;
 
 import com.base.BaseTest;
-import com.pageobjects.ContactPage;
 import com.pageobjects.HomePage;
 import org.testng.Assert;
 
-public class JupiterHomePageTest extends BaseTest {
+public class HomePageTest extends BaseTest {
     @org.testng.annotations.Test(description = "This test validates the Jupiter toys hope page title")
     public void testHomePage() {
         logger = extent.createTest("To verify Jupiter toys page title");
-        given("Driver calls the url");
-        when(" Home page pops up");
+        when(" Driver call the url");
         HomePage homePage = new HomePage(driver);
-        then("driver gets the title of the page");
+        then(" Driver gets the title of the page");
         Assert.assertEquals(homePage.getTitle(), "Jupiter Toys");
 
     }
